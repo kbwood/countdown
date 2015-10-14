e3923969984fbb1c22bbb5f61ec12b8f39f306ce
Alex Kozack <cawa-93@yandex.ru>
2015-10-14 14:27:09 +0300

    Updated UK local whichLabels method.

--- jquery.countdown-uk.js
+++ jquery.countdown-uk.js
@@ -8,7 +8,7 @@
 		labels2: ['Роки', 'Місяці', 'Тижні', 'Дні', 'Години', 'Хвилини', 'Секунди'],
 		compactLabels: ['r', 'm', 't', 'd'],
 		whichLabels: function(amount) {
-			return (amount == 1 ? 1 : (amount >=2 && amount <= 4 ? 2 : 0));
+			return amount !=0 ? (amount%10==1&&amount%100!=11?1:amount%10>=2&&amount%10<=4&&(amount%100<10||amount%100>=20)?2:0) : 0;
 		},
 		digits: ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'],
 		timeSeparator: ':', isRTL: false};
