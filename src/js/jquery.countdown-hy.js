d7b0c6c8d37abf6b203fe0859ad965246eae7df7
Arsen <arsen.hovhannisyan.1991@gmail.com>
2019-07-24 12:39:10 +0400

    Update jquery.countdown-hy.js
    - Add labels2
    - Fix grammar

--- src/js/jquery.countdown-hy.js
+++ src/js/jquery.countdown-hy.js
@@ -1,16 +1,18 @@
 /* http://keith-wood.name/countdown.html
  * Armenian initialisation for the jQuery countdown extension
- * Written by Artur Martirosyan. (artur{at}zoom.am) October 2011. */
+ * Written by Artur Martirosyan. (artur{at}zoom.am) October 2011.
+ * Edited By Arsen Hovhannisyan. (arsen.hovhannisyan.1991@gmail.am) Jul 2019. */
 (function($) {
 	'use strict';
-	$.countdown.regionalOptions.hy = {
-		labels: ['Տարի','Ամիս','Շաբաթ','Օր','Ժամ','Րոպե','Վարկյան'],
-		labels1: ['Տարի','Ամիս','Շաբաթ','Օր','Ժամ','Րոպե','Վարկյան'],
-		compactLabels: ['տ','ա','շ','օ'], 
+	$.countdown.regionalOptions.am = {
+		labels: ['Տարի','Ամիս','Շաբաթ','Օր','Ժամ','Րոպե','Վայրկյան'],
+		labels1: ['Տարի','Ամիս','Շաբաթ','Օր','Ժամ','Րոպե','Վայրկյան'],
+        labels2: ['Տարի','Ամիս','Շաբաթ','Օր','Ժամ','Րոպե','Վայրկյան'],
+        compactLabels: ['տ','ա','շ','օ'],
 		whichLabels: null,
 		digits: ['0','1','2','3','4','5','6','7','8','9'],
 		timeSeparator: ':',
 		isRTL: false
 	};
-	$.countdown.setDefaults($.countdown.regionalOptions.hy);
+	$.countdown.setDefaults($.countdown.regionalOptions.am);
 })(jQuery);
